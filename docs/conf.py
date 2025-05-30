e3ba36dbc126b1a8b5863eedcb6da2eb3991fe0a
Sadot Nahum Hernandez Moreno <sdhernan@procesar.com>
2025-05-30 00:27:14 -0600

    update

--- docs/conf.py
+++ docs/conf.py
@@ -1,25 +1,16 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'Thymeleaf FileInput with MinIO'
 copyright = '2025, Zademy'
 author = 'Zademy'
 release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
-    'sphinxcontrib.jquery',  # Añadido explícitamente para resolver problemas con jQuery
-    'sphinx_rtd_theme',
 ]
 
 templates_path = ['_templates']
@@ -27,20 +18,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 language = 'es'
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = 'sphinx_rtd_theme'
-html_static_path = []
-html_css_files = []
-html_js_files = []
 
-html_theme_options = {}
-html_show_sourcelink = False
-html_show_sphinx = False
+# Configuración mínima para evitar problemas con las fuentes
+html_static_path = []
 
 # -- Options for autodoc -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html
-
 # Mock dependencies that might not be available during documentation build
 autodoc_mock_imports = [
     'org.springframework',
@@ -52,8 +35,5 @@ autodoc_mock_imports = [
 ]
 
 # -- Options for ReadTheDocs -------------------------------------------------
-# https://docs.readthedocs.io/en/stable/config-file/v2.html
 html_show_sourcelink = False
-
-# If true, "Created using Sphinx" is shown in the HTML footer.
 html_show_sphinx = False
