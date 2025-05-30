b065657b7f5402d7cae6cd33475fa02a66656e85
Sadot Nahum Hernandez Moreno <sdhernan@procesar.com>
2025-05-30 00:12:59 -0600

    update

--- docs/conf.py
+++ docs/conf.py
@@ -3,9 +3,6 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# Importar el tema Bootstrap
-import sphinx_bootstrap_theme
-
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -22,7 +19,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
     'sphinxcontrib.jquery',  # Añadido explícitamente para resolver problemas con jQuery
-    # No es necesario añadir sphinx_bootstrap_theme como extensión si solo se usa como tema
+    'sphinx_rtd_theme',
 ]
 
 templates_path = ['_templates']
@@ -32,30 +29,11 @@ language = 'es'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# Configuración del tema Bootstrap
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
-
-# Opciones del tema Bootstrap
-html_theme_options = {
-    'navbar_title': "Thymeleaf FileInput with MinIO",
-    'bootswatch_theme': "united",  # Tema de Bootswatch (ej. "flatly", "cerulean", "cosmo", etc.)
-    'navbar_sidebarrel': True,
-    'navbar_links': [
-        ("Inicio", "index", True),
-        ("Configuración", "configuration", True),
-        ("Uso", "usage", True),
-        ("Comenzar", "getting-started", True),
-    ],
-    'navbar_pagenav': True,
-    'navbar_pagenav_name': "En esta página",
-    'globaltoc_depth': 2,
-    'globaltoc_includehidden': False,
-    'source_link_position': None,
-    'bootstrap_version': "3",
-}
-
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# Añadir archivos JavaScript personalizados
+html_js_files = ['js/sphinx_rtd_theme_fix.js']
 
 # -- Options for autodoc -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html
